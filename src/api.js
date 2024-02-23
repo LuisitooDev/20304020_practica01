d9d56089f8dc02845ce6cf0108a1dd1d537815b2
LuisitooDev <laha11590@hotmail.com>
2024-02-22 20:22:34 -0700

    Error

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ app.get('/issues', (req, res) => {
 
 // Get all tasks
 app.get('/tasks', (req, res) => {
-  taskRepository.getAll();
+  const tasks = []//taskRepository.getAll();
   res.json(tasks);
 });
 
--- src/api.test.js
+++ src/api.test.js
@@ -1,11 +1,10 @@
 const request = require('supertest');
 const app = require('./api'); 
-
 // Test the /tasks GET endpoint
 test('GET /tasks returns all tasks', async () => {
   const response = await request(app).get('/tasks');
   expect(response.status).toBe(200);
-  expect(response.body).toHaveLength(3); // Assuming there are 2 tasks initially
+  expect(response.body).toHaveLength(2); // Assuming there are 2 tasks initially
 });
 
 // Test the /tasks GET by ID endpoint

d325e3784d220e095411019280af2711e8c65b73
LuisitooDev <laha11590@hotmail.com>
2024-02-22 23:40:05 -0700

    error de permisos listo

--- src/api.test.js
+++ src/api.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./api'); 
 
 // Test the /tasks GET endpoint
-test('GET /tasks returns all tasks', async () => {
+test('GET /tasks returns all tasks.', async () => {
   const response = await request(app).get('/tasks');
   expect(response.status).toBe(200);
   expect(response.body).toHaveLength(3); // Assuming there are 2 tasks initially
